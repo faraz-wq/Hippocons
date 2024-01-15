0077a53ac2a70425b927039ce6b9fa9d2b4ff854
M1sm4tch <91552196+M1sm4tch@users.noreply.github.com>
2024-01-15 22:51:14 +0530

    working on Collections
    created orders, media, productfile
    fixed bug in media, performed payload type generation

---
+++ src/collections/Orders.ts
@@ -0,0 +1,54 @@
+import { Access, CollectionConfig } from "payload/types";
+
+const YourOwn: Access = ({req: {user}}) => {
+    if(user.role === 'admin') return true
+    return {
+        user: {
+            equals: user?.id,
+        }
+    }
+}
+
+export const Orders: CollectionConfig = {
+    slug: "orders",
+    admin: {
+        useAsTitle: "Your Orders",
+        description: "A summary of your orders on HippoCons"
+    },
+    access:{
+        read: YourOwn,
+        update: ({req})=> req.user.role === "admin",
+        create: ({req})=> req.user.role === "admin",
+        delete: ({req})=> req.user.role === "admin",
+    },
+    fields: [
+        {
+            name: "_isPaid",
+            type: "checkbox",
+            access: {
+                read: ({req}) => req.user.role ==='admin',
+                create: ()=> false,
+                update: ()=> false
+            },
+            admin:{
+                hidden: true,
+            },
+            required: true,
+        },
+        {
+            name:"user",
+            type:"relationship",
+            admin: {
+                hidden:true
+            },
+            relationTo: "users",
+            required: true,
+        },
+        {
+            name:"products",
+            type: "relationship",
+            relationTo:"products",
+            hasMany: true,
+        },
+    ],
+}
