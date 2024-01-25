92305227b62b62ccb61c8b080a75f5c2bd7e055d
M1sm4tch <91552196+M1sm4tch@users.noreply.github.com>
2024-01-25 22:12:57 +0530

    image fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-			},
-		],
+		domains: [
+			'localhost',
+			'hippocons-production.up.railway.app',
+		]
 	},
 };
 
